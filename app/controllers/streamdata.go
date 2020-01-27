fad633cd0748c571eb0dd0792e40f6e7ed1c72b9
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-01-27 19:19:12 +0900

    Serverの立ち上げ

--- app/controllers/streamdata.go
+++ app/controllers/streamdata.go
@@ -11,13 +11,15 @@ func StreamIngestionData() {
 	var tickerChanel = make(chan bitflyer.Ticker)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChanel)
-	for ticker := range tickerChanel {
-		log.Printf("action=StreamIngestionData, %v", ticker)
-		for _, duration := range config.Config.Durations {
-			isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
-			if isCreated == true && duration == config.Config.TradeDuration {
-				// TODO
+	go func() {
+		for ticker := range tickerChanel {
+			log.Printf("action=StreamIngestionData, %v", ticker)
+			for _, duration := range config.Config.Durations {
+				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
+				if isCreated == true && duration == config.Config.TradeDuration {
+					// TODO
+				}
 			}
 		}
-	}
+	}()
 }
--- main.go
+++ main.go
@@ -1,15 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"ready_go/app/controllers"
-	"ready_go/app/models"
 	"ready_go/config"
 	"ready_go/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	fmt.Println(models.DbConnection)
 	controllers.StreamIngestionData()
+	controllers.StartWebServer()
 }
