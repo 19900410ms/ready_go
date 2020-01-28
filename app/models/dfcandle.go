52f8a65ecfd0bbf31c94aa4479995211be348d09
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-01-28 16:06:56 +0900

    Bollinger Bandsアルゴリズム

--- app/models/dfcandle.go
+++ app/models/dfcandle.go
@@ -12,6 +12,7 @@ type DataFrameCandle struct {
 	Candles     []Candle      `json:"candles"`
 	Smas        []Sma         `json:"smas,omitempty"`
 	Emas        []Ema         `json:"emas,omitempty"`
+	BBands      *BBands       `json:"bbands,omitempty"`
 }
 
 type Sma struct {
@@ -22,6 +23,14 @@ type Sma struct {
 type Ema struct {
 	Period int       `json:"period,omitempty"`
 	Values []float64 `json:"values,omitempty"`
+}
+
+type BBands struct {
+	N    int       `json:"n,omitempty"`
+	K    float64   `json:"k,omitempty"`
+	Up   []float64 `json:"up,omitempty"`
+	Mid  []float64 `json:"mid,omitempty"`
+	Down []float64 `json:"down,omitempty"`
 }
 
 func (df *DataFrameCandle) Times() []time.Time {
@@ -85,10 +94,25 @@ func (df *DataFrameCandle) AddSma(period int) bool {
 
 func (df *DataFrameCandle) AddEma(period int) bool {
 	if len(df.Candles) > period {
-		df.Smas = append(df.Smas, Sma{
+		df.Emas = append(df.Emas, Ema{
 			Period: period,
 			Values: talib.Ema(df.Closes(), period),
 		})
+		return true
+	}
+	return false
+}
+
+func (df *DataFrameCandle) AddBBands(n int, k float64) bool {
+	if n <= len(df.Closes()) {
+		up, mid, down := talib.BBands(df.Closes(), n, k, k, 0)
+		df.BBands = &BBands{
+			N:    n,
+			K:    k,
+			Up:   up,
+			Mid:  mid,
+			Down: down,
+		}
 		return true
 	}
 	return false
