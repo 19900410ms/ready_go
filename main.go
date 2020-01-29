c956d3cd6c0df882aae72b04bd2198e1eb612e25
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-01-29 16:03:44 +0900

    eventの表示

--- main.go
+++ main.go
@@ -1,32 +1,15 @@
 package main
 
-// import (
-// 	"ready_go/app/controllers"
-// 	"ready_go/config"
-// 	"ready_go/utils"
-// )
-
 import (
-	"fmt"
-	"ready_go/app/models"
-	"time"
+	"log"
+	"ready_go/app/controllers"
+	"ready_go/config"
+	"ready_go/utils"
 )
 
-// func main() {
-// 	utils.LoggingSettings(config.Config.LogFile)
-// 	controllers.StreamIngestionData()
-// 	controllers.StartWebServer()
-// }
-
 func main() {
-	s := models.NewSignalEvents()
-	df, _ := models.GetAllCandle("BTC_USD", time.Minute, 10)
-	c1 := df.Candles[0]
-	c2 := df.Candles[5]
-	s.Buy("BTC_USD", c1.Time.UTC(), c1.Close, 1.0, true)
-	s.Sell("BTC_USD", c2.Time.UTC(), c2.Close, 1.0, true)
-	fmt.Println(models.GetSignalEventsByCount(1))
-	fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
-	fmt.Println(s.CollectAfter(time.Now().UTC()))
-	fmt.Println(s.CollectAfter(c1.Time))
+	utils.LoggingSettings(config.Config.LogFile)
+	controllers.StreamIngestionData()
+	controllers.StartWebServer()
+	log.Println(controllers.StartWebServer())
 }
