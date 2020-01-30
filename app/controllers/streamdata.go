f90b8163cff7ceb24f2a28abfe5a5fab1b4047e7
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-01-30 11:08:04 +0900

    自動売買アルゴリズム

--- app/controllers/streamdata.go
+++ app/controllers/streamdata.go
@@ -8,16 +8,19 @@ import (
 )
 
 func StreamIngestionData() {
-	var tickerChanel = make(chan bitflyer.Ticker)
+	c := config.Config
+	ai := NewAI(c.ProductCode, c.TradeDuration, c.DataLimit, c.UsePercent, c.StopLimitPercent, c.BackTest)
+
+	var tickerChannl = make(chan bitflyer.Ticker)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChanel)
+	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannl)
 	go func() {
-		for ticker := range tickerChanel {
+		for ticker := range tickerChannl {
 			log.Printf("action=StreamIngestionData, %v", ticker)
 			for _, duration := range config.Config.Durations {
 				isCreated := models.CreateCandleWithDuration(ticker, ticker.ProductCode, duration)
 				if isCreated == true && duration == config.Config.TradeDuration {
-					// TODO
+					ai.Trade()
 				}
 			}
 		}
