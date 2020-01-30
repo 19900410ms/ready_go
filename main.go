50ea07dbd2689bf4c5f9289c0e2c40fe6e47538a
19900410ms <57243729+19900410ms@users.noreply.github.com>
2020-01-30 10:29:50 +0900

    相場にマッチするインディケーター選出

--- main.go
+++ main.go
@@ -10,6 +10,5 @@ import (
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	controllers.StreamIngestionData()
-	controllers.StartWebServer()
 	log.Println(controllers.StartWebServer())
 }
